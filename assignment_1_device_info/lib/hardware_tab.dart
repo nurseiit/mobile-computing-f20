1983ee5443e7c94066bfcb564797704f63ea1e23
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-09-21 19:46:19 +0600

    ✏️ Fix extra ; typo

--- assignment_1_device_info/lib/hardware_tab.dart
+++ assignment_1_device_info/lib/hardware_tab.dart
@@ -4,7 +4,7 @@ import 'package:system_info/system_info.dart';
 class HardwareTab extends StatelessWidget {
   static const int MEGABYTE = 1024 * 1024;
 
-  final Map<String, dynamic> _deviceData = <String, dynamic> {
+  final Map<String, dynamic> _deviceData = <String, dynamic>{
     'Kernel architecture': SysInfo.kernelArchitecture,
     'Kernel bitness': SysInfo.kernelBitness,
     'Kernel name': SysInfo.kernelName,
@@ -16,16 +16,12 @@ class HardwareTab extends StatelessWidget {
     'User name': SysInfo.userName,
     'User space bitness': SysInfo.userSpaceBitness,
     'Total physical memory':
-    '${SysInfo.getTotalPhysicalMemory() ~/ MEGABYTE} MB',
-    'Free physical memory':
-    '${SysInfo.getFreePhysicalMemory() ~/ MEGABYTE} MB',
-    'Total virtual memory':
-    '${SysInfo.getTotalVirtualMemory() ~/ MEGABYTE} MB',
-    'Free virtual memory':
-    '${SysInfo.getFreeVirtualMemory() ~/ MEGABYTE} MB',
-    'Virtual memory size':
-    '${SysInfo.getVirtualMemorySize() ~/ MEGABYTE} MB',
-  };;
+        '${SysInfo.getTotalPhysicalMemory() ~/ MEGABYTE} MB',
+    'Free physical memory': '${SysInfo.getFreePhysicalMemory() ~/ MEGABYTE} MB',
+    'Total virtual memory': '${SysInfo.getTotalVirtualMemory() ~/ MEGABYTE} MB',
+    'Free virtual memory': '${SysInfo.getFreeVirtualMemory() ~/ MEGABYTE} MB',
+    'Virtual memory size': '${SysInfo.getVirtualMemorySize() ~/ MEGABYTE} MB',
+  };
 
   @override
   Widget build(BuildContext context) {
