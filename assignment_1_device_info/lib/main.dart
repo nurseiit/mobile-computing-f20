34d3e7991ed082f242d22770f4b45fd068538c10
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-09-21 13:52:10 +0600

    🎨 Setup basic structure for device tab

--- assignment_1_device_info/lib/main.dart
+++ assignment_1_device_info/lib/main.dart
@@ -40,11 +40,49 @@ class HomePage extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            Icon(Icons.directions_car),
+            DeviceInfoDisplay(),
             Icon(Icons.directions_transit),
             Icon(Icons.directions_bike),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class DeviceInfoDisplay extends StatelessWidget {
+  final List<String> categories = [
+    'Model',
+    'Brand',
+    'Manufacturer',
+    'Board',
+    'Bootloader',
+    'Device',
+    'Display',
+    'Hardware',
+    'Host',
+    'ID',
+    'Product'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20.0, 20.0, 0.0, 0.0),
+      child: Table(
+        children: categories
+            .map(
+              (category) => TableRow(children: [
+                Padding(
+                  child: Text(category),
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                ),
+                Padding(
+                  child: Text('kjkjskdjkjsdkjsdjkj'),
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                ),
+              ]),
+            )
+            .toList(),
       ),
     );
   }
